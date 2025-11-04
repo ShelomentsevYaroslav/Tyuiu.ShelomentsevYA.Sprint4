982fb5e4801d7cd6268085d0aa99b9f7709dd8b6
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-11-04 15:29:44 +0500

    Task1

---
+++ Tyuiu.ShelomentsevYA.Sprint4.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShelomentsevYA.Sprint4.Task1.V29.Lib;
+
+namespace Tyuiu.ShelomentsevYA.Sprint4.Task1.V29.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void Calculate_SumOdd_Ok()
+        {
+            var ds = new DataService();
+            int[] arr = { 5, 3, 3, 4, 1, 3, 3, 5, 6, 2, 7, 2, 1, 8 };
+            int expected = 31; 
+            int actual = ds.Calculate(arr);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Tyuiu.ShelomentsevYA.Sprint4.Task1.V29/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Tyuiu.ShelomentsevYA.Sprint4.Task1.V29.Lib;
+
+namespace Tyuiu.ShelomentsevYA.Sprint4.Task1.V29
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #4 | Выполнил: Шеломенцев Я.А. | ИСТНб-25-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #29                                                             *");
+            Console.WriteLine("* Выполнил: Шеломенцев Я.А. | ИСТНб-25-1                                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дан массив из 14 целых чисел (1..9). Подсчитать сумму нечётных элементов*");
+            Console.WriteLine("***************************************************************************\n");
+
+            const int n = 14;
+            int[] arr = new int[n];
+
+            Console.WriteLine("Введите 14 целых чисел через Enter (1..9).");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write($"[{i + 1}/{n}] = ");
+
+                while (!int.TryParse(Console.ReadLine(), out arr[i]))
+                {
+                    Console.Write("Некорректно. Повторите ввод: ");
+                }
+            }
+
+            var ds = new DataService();
+            int result = ds.Calculate(arr);
+
+            Console.WriteLine("\n***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
