58db6da443b1e8834d966fba62c43815514cc671
Ярослав Шеломенцев <Tyuiu.ShelomentsevYA@outlook.com>
2025-11-04 15:20:11 +0500

    Task0

--- Tyuiu.ShelomentsevYA.Sprint4.Task0.V10.Lib/DataService.cs
+++ Tyuiu.ShelomentsevYA.Sprint4.Task0.V10.Lib/DataService.cs
@@ -8,7 +8,7 @@ namespace Tyuiu.ShelomentsevYA.Sprint4.Task0.V10.Lib
             int sum = 0;
             for (int i = 0; i < array.Length; i++)
             {
-                if ((array[i] & 1) != 0) 
+                if ((array[i] & 1) == 1) 
                     sum += array[i];
             }
             return sum;
